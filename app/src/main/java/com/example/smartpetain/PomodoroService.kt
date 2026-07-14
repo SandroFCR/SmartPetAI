85db9b844063b1f8004d53f01513e44d68c25e5c
Sandro <sandrofelipecusihuamanrojas@gmail.com>
2026-07-14 08:33:56 -0500

    e

--- app/src/main/java/com/example/smartpetain/PomodoroService.kt
+++ app/src/main/java/com/example/smartpetain/PomodoroService.kt
@@ -56,7 +56,7 @@ class PomodoroService : Service() {
                 blockedPackages = emptySet()
                 pauseTimer()
                 stopForeground(STOP_FOREGROUND_REMOVE)
-gitt                val nm = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+              val nm = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
                 nm.cancel(2001) // Explicitly cancel the timer notification
                 stopSelf()
             }
